6c62675262e62e28f1b4eb50dbc90b425ae502c1
bernardanderson <bernard.anderson@gmail.com>
2016-12-02 15:05:22 -0600

    Added 'Photos' model

---
+++ Models/Photos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eyesonthenet.Models
+{
+    public class Photos
+    {
+        [Key]
+        public int PhotoId { get; set; }
+        public string Filename { get; set; }
+        public string FileLocation{ get; set; }
+        public DateTime CreationDate { get; set; }
+        public Cameras CameraId { get; set; }
+
+    }
+}
