ef69f551218aa7ba77fd5929dc3bba54319b8247
bernardanderson <bernard.anderson@gmail.com>
2016-12-07 19:12:28 -0600

    Added JWT Code for Authorization I followed this site for incorporating JWT into your code. https://stormpath.com/blog/token-authentication-asp-net-core Some additional tweaking will need to be done (like removing the hardcoded username/pass)

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using EyesOnTheNet.Private;
+using EyesOnTheNet.TokenProvider;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Options;
 
 namespace EyesOnTheNet
 {
@@ -32,9 +36,63 @@ namespace EyesOnTheNet
             services.AddMvc();
         }
 
+        private static readonly string secretKey = PrivateParameters.JWTSecretKey;
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            ///// Start of JWT Authentication Middleware
+            app.UseStaticFiles();
+
+            var signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secretKey)); // secretKey is Private and known to the server ONLY
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                // The signing key must match!
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+
+                // Validate the JWT Issuer (iss) claim
+                ValidateIssuer = true,
+                ValidIssuer = "EotWServer",
+
+                // Validate the JWT Audience (aud) claim
+                ValidateAudience = true,
+                ValidAudience = "EotWUser",
+
+                // Validate the token expiry
+                ValidateLifetime = true,
+
+                // If you want to allow a certain amount of clock drift, set that here:
+                ClockSkew = TimeSpan.Zero
+            };
+
+            app.UseJwtBearerAuthentication(new JwtBearerOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                TokenValidationParameters = tokenValidationParameters
+            });
+
+            // Add JWT generation endpoint:
+            var options = new TokenProviderOptions
+            {
+                Audience = "EotWUser",
+                Issuer = "EotWServer",
+                SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256),
+            };
+
+            app.UseMiddleware<TokenProviderMiddleware>(Options.Create(options));
+
+            // Techincally not part of the JWT Middleware, but needed to allow CORS access for the
+            //  custom headers I'm using.
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin().WithMethods("GET", "OPTIONS").WithHeaders("Authorization")
+            );
+
+            ///// End of JWT Authentication Middleware
+
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
---
+++ TokenProvider/TokenProviderOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace EyesOnTheNet.TokenProvider
+{
+    public class TokenProviderOptions
+    {
+        public string Path { get; set; } = "/token";  // Sets the API pathname
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public TimeSpan Expiration { get; set; } = TimeSpan.FromDays(1); // Sets the Auth TimeLimit
+        public SigningCredentials SigningCredentials { get; set; }
+    }
+}
