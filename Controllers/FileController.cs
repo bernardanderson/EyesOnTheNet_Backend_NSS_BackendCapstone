8516115bca23aeded0aee758bd335b94c5cddc89
bernardanderson <bernard.anderson@gmail.com>
2016-12-25 23:28:51 -0600

    Added ability to record any one of a users cams

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using EyesOnTheNet.DAL;
 using System.IdentityModel.Tokens.Jwt;
 using EyesOnTheNet.Models;
@@ -12,22 +8,13 @@ using EyesOnTheNet.Models;
 namespace EyesOnTheNet.Controllers
 {
     public class FileController : Controller
-    {        // GET api/values/5
-        [HttpGet("api/[controller]")]
-        public IActionResult TestController()
-        {
-            return Ok("This Controller Works");
-        }
-
-        // GET api/values/5
-        [HttpGet("api/[controller]/{cameraId:int}")]
-        public async Task<IActionResult> SaveSingleCameraPicture(int cameraId)
+    {
+        // GET api/file/5
+        [HttpGet("api/[controller]/{cameraId:int}/{timerInterval:int}")]
+        public void SaveSingleCameraPicture(int cameraId, int timerInterval)
         {
             string currentUser = new JwtSecurityToken(Request.Cookies["access_token"]).Subject;
-            Camera returnedCamera = new EyesOnTheNetRepository().CanAccessThisCamera(currentUser, cameraId);
-            string PictureReponse = await new FileRequests().SaveCameraPhoto(returnedCamera);
-
-            return Ok(PictureReponse);
+            new FileRequests(currentUser, cameraId).StartTimer(timerInterval);
         }
     }
 }
--- Controllers/FileRequests.cs
+++ Controllers/FileRequests.cs
@@ -1,24 +1,36 @@
-﻿using EyesOnTheNet.Models;
+﻿using EyesOnTheNet.DAL;
+using EyesOnTheNet.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace EyesOnTheNet.Controllers
 {
     public class FileRequests
     {
-        public async Task<string> SaveCameraPhoto(Camera sentCamera)
+        public FileRequests(string sentUserName, int sentCameraId)
+        {
+            userName = sentUserName;
+            userCamera = new EyesOnTheNetRepository().CanAccessThisCamera(sentUserName, sentCameraId);
+        }
+
+        private string userName { get; set; }
+        private Camera userCamera { get; set; }
+
+        public void StartTimer(int timerInterval)
+        {
+            TimerCallback newCallback = new TimerCallback(SaveCameraPhoto);
+            Timer newTimer = new Timer(newCallback, null, 0, timerInterval);
+
+        }
+        private async void SaveCameraPhoto(object obj)
         {
             CameraRequests myCameraRequest = new CameraRequests();
-            Picture singleCameraPicture = await myCameraRequest.GetSnapshot(sentCamera);
+            Picture singleCameraPicture = await myCameraRequest.GetSnapshot(userCamera);
 
             long currentDateTime = DateTimeOffset.Now.ToUnixTimeSeconds();
-            string newSaveString = $"{sentCamera.CameraId.ToString()}_{currentDateTime.ToString()}.jpg";
+            string newSaveString = $"{userCamera.CameraId.ToString()}_{currentDateTime.ToString()}.jpg";
             File.WriteAllBytes($"/home/banderso/NSS_Backend/eyesonthenet/images/{newSaveString}", singleCameraPicture.data);
-
-            return newSaveString;
         }
     }
 }
